b39838a0f12c0876c14271964930246e740172b0
katherin <ft.katheochoa@gmail.com>
2021-11-23 17:53:21 -0500

    added button delete

--- React-calculator/my-app/src/App.js
+++ React-calculator/my-app/src/App.js
@@ -17,8 +17,16 @@ export const ACTIONS = {
 function reducer(state, { type, payload }) {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
-      //Current state : Means don't make any changes.
-      if (payload.digit === "0" && state.currentOperand === "0") return state;
+      if (state.ovewrite) {
+        return {
+          ...state,
+          currentOperand: payload.digit,
+          ovewrite: false,
+        };
+      }
+      if (payload.digit === "0" && state.currentOperand === "0")
+        //Current state : Means don't make any changes.
+        return state;
       if (payload.digit === "." && state.currentOperand.includes("."))
         return state;
       return {
@@ -31,6 +39,13 @@ function reducer(state, { type, payload }) {
         return state;
       }
 
+      // Ovewrite the operation
+      if (state.currentOperand == null) {
+        return {
+          ...state,
+          operation: payload.operation,
+        };
+      }
       if (state.previousOperand == null) {
         return {
           ...state,
@@ -50,6 +65,43 @@ function reducer(state, { type, payload }) {
     // This clear the input.
     case ACTIONS.CLEAR:
       return {};
+
+    case ACTIONS.DELETE_DIGIT:
+      if (state.ovewrite) {
+        return {
+          ...state,
+          ovewrite: false,
+          currentOperand: null,
+        };
+      }
+
+      if (state.currentOperand == null) return state;
+      if (state.currentOperand.length === 1) {
+        return { ...state, currentOperand: null };
+      }
+      // Remover el ultimo digito.
+      return {
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1),
+      };
+    // = Operation
+
+    case ACTIONS.EVALUATE:
+      if (
+        state.operation == null ||
+        state.currentOperand == null ||
+        state.previousOperand == null
+      ) {
+        return state;
+      }
+
+      return {
+        ...state,
+        ovewrite: true,
+        previousOperand: null,
+        operation: null,
+        currentOperand: evaluate(state),
+      };
   }
 }
 
@@ -98,7 +150,9 @@ function App() {
       >
         AC
       </button>
-      <button>DEL</button>
+      <button onClick={() => dispatch({ type: ACTIONS.DELETE_DIGIT })}>
+        DEL
+      </button>
       <OperationButton operation="÷" dispatch={dispatch} />
       <DigitButton digit="1" dispatch={dispatch} />
       <DigitButton digit="2" dispatch={dispatch} />
@@ -114,7 +168,12 @@ function App() {
       <OperationButton operation="-" dispatch={dispatch} />
       <DigitButton digit="." dispatch={dispatch} />
       <DigitButton digit="0" dispatch={dispatch} />
-      <button className="span-two">=</button>
+      <button
+        className="span-two"
+        onClick={() => dispatch({ type: ACTIONS.EVALUATE })}
+      >
+        =
+      </button>
     </div>
   );
 }

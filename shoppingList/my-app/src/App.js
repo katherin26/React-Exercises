3d4f68964afefe4feb23117d7d174a65d45576e8
katherin <ft.katheochoa@gmail.com>
2021-12-02 18:25:15 -0500

    added btn onRemove

--- shoppingList/my-app/src/App.js
+++ shoppingList/my-app/src/App.js
@@ -20,12 +20,25 @@ function App() {
     }
   };
 
+  const onRemove = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== product.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+        )
+      );
+    }
+  };
+
   return (
     <div className="App">
       <Header />
       <div className="row">
         <Main onAdd={onAdd} products={products} />
-        <Basket onAdd={onAdd} cartItems={cartItems} />
+        <Basket onAdd={onAdd} onRemove={onRemove} cartItems={cartItems} />
       </div>
     </div>
   );

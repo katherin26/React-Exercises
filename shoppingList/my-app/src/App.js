3a2b139f67a5a7a6482ffb551cff3ea90a612305
katherin <ft.katheochoa@gmail.com>
2021-12-02 17:42:57 -0500

    added onClick

--- shoppingList/my-app/src/App.js
+++ shoppingList/my-app/src/App.js
@@ -2,15 +2,30 @@ import Basket from "./components/Basket";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import data from "./data";
+import { useState } from "react";
 
 function App() {
   const { products } = data;
+  const [cartItems, setCartItems] = useState([]);
+  const onAdd = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+    }
+  };
+
   return (
     <div className="App">
       <Header />
       <div className="row">
-        <Main products={products} />
-        <Basket />
+        <Main onAdd={onAdd} products={products} />
+        <Basket onAdd={onAdd} cartItems={cartItems} />
       </div>
     </div>
   );

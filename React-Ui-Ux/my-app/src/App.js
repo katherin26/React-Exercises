0a159c3973078acf17814264a8c7b3342c5299a5
katherin <ft.katheochoa@gmail.com>
2021-12-03 21:23:33 -0500

    added new project and folder components

---
+++ React-Ui-Ux/my-app/src/App.js
@@ -0,0 +1,18 @@
+// rafce = React functional component
+
+import React from "react";
+import Article from "./components/article/Article";
+import brand from "./components/brand/Brand";
+import cta from "./components/cta/Cta";
+import feature from "./components/feature/Feature";
+import navbar from "./components/navbar/Navbar";
+
+const App = () => {
+  return (
+    <div>
+      <h1>GPT-3</h1>
+    </div>
+  );
+};
+
+export default App;

cc399b60a11dacac43dc4fb71d6dc954a01dbd57
katherin <ft.katheochoa@gmail.com>
2021-12-03 21:43:42 -0500

    added containers folders

--- React-Ui-Ux/my-app/src/App.js
+++ React-Ui-Ux/my-app/src/App.js
@@ -1,12 +1,5 @@
 // rafce = React functional component
 
-import React from "react";
-import Article from "./components/article/Article";
-import brand from "./components/brand/Brand";
-import cta from "./components/cta/Cta";
-import feature from "./components/feature/Feature";
-import navbar from "./components/navbar/Navbar";
-
 const App = () => {
   return (
     <div>
--- React-Ui-Ux/my-app/src/components/index.js
+++ React-Ui-Ux/my-app/src/components/index.js
@@ -1,1 +1,9 @@
 export { default as Article } from "./article/Article";
+export { default as Brand } from "./brand/Brand";
+export { default as Cta } from "./cta/Cta";
+export { default as Feature } from "./feature/Feature";
+export { default as Navbar } from "./navbar/Navbar";
+
+//WIth all this we can now import in 1 single line in de App.js
+
+// import { Article, Brand, Cta } from "./components";

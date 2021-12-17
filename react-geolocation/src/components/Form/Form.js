32656b329f00d6cbb039a67b1094adf841ec4e52
katherin <ft.katheochoa@gmail.com>
2021-12-16 21:57:52 -0500

    refactoring

--- react-geolocation/src/components/Form/Form.js
+++ react-geolocation/src/components/Form/Form.js
@@ -1,22 +1,16 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import IconButton from "@mui/material/IconButton";
-import Input from "@mui/material/Input";
-import FilledInput from "@mui/material/FilledInput";
+
 import OutlinedInput from "@mui/material/OutlinedInput";
-import InputLabel from "@mui/material/InputLabel";
+
 import InputAdornment from "@mui/material/InputAdornment";
 import FormHelperText from "@mui/material/FormHelperText";
 import FormControl from "@mui/material/FormControl";
-import TextField from "@mui/material/TextField";
-import Visibility from "@mui/icons-material/Visibility";
-import VisibilityOff from "@mui/icons-material/VisibilityOff";
+
 import MenuItem from "@mui/material/MenuItem";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
+
 import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
+
 import { Select } from "@mui/material";
 
 const workoutList = [

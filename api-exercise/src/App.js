b6d6bf2e93e79196a8aeb5c3f1df1d549486df50
katherin <ft.katheochoa@gmail.com>
2022-01-07 15:15:37 -0500

    added new project with air quality

---
+++ api-exercise/src/App.js
@@ -0,0 +1,12 @@
+import Content from "./components/Content/Content";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Content />
+    </div>
+  );
+}
+
+export default App;
---
+++ api-exercise/src/components/Content/Content.js
@@ -0,0 +1,18 @@
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+
+function Content() {
+  return (
+    <Container>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      ></Box>
+    </Container>
+  );
+}
+
+export default Content;

9ce48f6df479e2a17f1033057cdf5849cb978d1a
katherin <ft.katheochoa@gmail.com>
2022-01-11 18:21:02 -0500

    Api getAirquality

--- react-geolocation/src/services/api.js
+++ react-geolocation/src/services/api.js
@@ -9,3 +9,20 @@ export function getWorkouts() {
 export function createWorkout(workout) {
   return axios.post(`${apiUrl}/workouts`, workout);
 }
+
+export async function getAirQuality() {
+  const response = await axios.get(
+    `https://air-quality.p.rapidapi.com/current/airquality`,
+    {
+      params: {
+        lat: "403746746",
+        lon: "-743030246",
+      },
+      headers: {
+        "x-rapidapi-host": "air-quality.p.rapidapi.com",
+        "x-rapidapi-key": "7b760b1092mshe837c1179b68733p13c26bjsn905c8dda768f",
+      },
+    }
+  );
+  return response.data;
+}

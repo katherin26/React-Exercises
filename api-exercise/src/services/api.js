c795662bb19b3185661264c15a1ffa45e3f777d1
katherin <ft.katheochoa@gmail.com>
2022-01-10 21:15:58 -0500

    small changes

--- api-exercise/src/services/api.js
+++ api-exercise/src/services/api.js
@@ -4,7 +4,7 @@ export async function getAirQuality() {
   const response = await axios.get(
     `https://air-quality.p.rapidapi.com/current/airquality`,
     {
-      params: { lat: "35.779", lon: "-78.638", hours: "72" },
+      params: { lat: "30.779", lon: "-78.638", hours: "72" },
       headers: {
         "x-rapidapi-host": "air-quality.p.rapidapi.com",
         "x-rapidapi-key": "7b760b1092mshe837c1179b68733p13c26bjsn905c8dda768f",

85a4d883f23f65e08b9383d336305def0db66e34
katherin <ft.katheochoa@gmail.com>
2022-01-12 18:41:45 -0500

    Added components folder

--- react-realstateapp/pages/index.js
+++ react-realstateapp/pages/index.js
@@ -1,12 +1,17 @@
+//Next allow you to create a function at the bottom of your component
+//and right inside of here we can do the api calls.
+//this line is taken from the parameters in the rapidHub = 5002&purpose=for-sale&hitsPerPage=6
+
+//In return  props : what next.js does is it automatically adds these to the props of our component right at the top
+//so right here we can destructure propertiesForSale and propertiesForRent
+
 import Link from "next/link";
 import Image from "next/image";
 import { Flex, Box, Text, Button } from "@chakra-ui/react";
 
-//Reusable Banner
-//This purpose is equal to props , that's why we desctructure them and get the purpose like this
-//({purpose}).
+import { baseUrl, fetchApi } from "../utils/fetchApi";
 
-const Banner = ({
+export const Banner = ({
   purpose,
   title1,
   title2,
@@ -16,13 +21,13 @@ const Banner = ({
   linkName,
   imageUrl,
 }) => (
-  <Flex flexwrap="wrap" justifyContent="center" alignItems="center" m="10">
-    <Image src={imageUrl} width={500} height={300} alt="banner" />
+  <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="10">
+    <Image src={imageUrl} width={500} height={300} />
     <Box p="5">
       <Text color="gray.500" fontSize="sm" fontWeight="medium">
         {purpose}
       </Text>
-      <Text fontSize="3x1" fontWeight="bold">
+      <Text fontSize="3xl" fontWeight="bold">
         {title1}
         <br />
         {title2}
@@ -32,40 +37,64 @@ const Banner = ({
         <br />
         {desc2}
       </Text>
-      <Button fontSize="xl">
-        <Link href={linkName}>{buttonText}</Link>
+      <Button fontSize="xl" bg="blue.300" color="white">
+        <Link href={linkName}>
+          <a>{buttonText}</a>
+        </Link>
       </Button>
     </Box>
   </Flex>
 );
 
-export default function Home() {
-  return (
-    <Box>
-      <h1>Hello World!!</h1>
-      <Banner
-        purpose="RENT A HOME"
-        title1="Rental Homes for"
-        title2="Everyone"
-        desc1="Explore Apartments, Villas, Homes"
-        desc2="and more"
-        buttonText="Explore Renting"
-        linkName="/search?purpose=for-rent"
-        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
-      />
+const Home = ({ propertiesForSale, propertiesForRent }) => (
+  <Box>
+    <Banner
+      purpose="RENT A HOME"
+      title1="Rental Homes for"
+      title2="Everyone"
+      desc1=" Explore from Apartments, builder floors, villas"
+      desc2="and more"
+      buttonText="Explore Renting"
+      linkName="/search?purpose=for-rent"
+      imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+    />
+    <Flex flexWrap="wrap">
+      {propertiesForRent.map((property) => (
+        <Property property={property} key={property.id} />
+      ))}
+    </Flex>
+    <Banner
+      purpose="BUY A HOME"
+      title1=" Find, Buy & Own Your"
+      title2="Dream Home"
+      desc1=" Explore from Apartments, land, builder floors,"
+      desc2=" villas and more"
+      buttonText="Explore Buying"
+      linkName="/search?purpose=for-sale"
+      imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
+    />
+    <Flex flexWrap="wrap">
+      {propertiesForSale.map((property) => (
+        <Property property={property} key={property.id} />
+      ))}
+    </Flex>
+  </Box>
+);
 
-      <Flex>{/*Fetch the properties and map over them ... */}</Flex>
-      <Banner
-        purpose="BUY A HOME"
-        title1="Find, Buy & Own Your"
-        title2="Drean Home"
-        desc1="Explore Apartments, Villas, Homes"
-        desc2="and more"
-        buttonText="Explore Buying"
-        linkName="/search?purpose=for-sale"
-        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
-      />
-      {/*Fetch the properties and map over them ... */}
-    </Box>
+export async function getStaticProps() {
+  const propertyForSale = await fetchApi(
+    `${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-sale&hitsPerPage=6`
   );
+  const propertyForRent = await fetchApi(
+    `${baseUrl}/properties/list?locationExternalIDs=5002&purpose=for-rent&hitsPerPage=6`
+  );
+
+  return {
+    props: {
+      propertiesForSale: propertyForSale?.hits,
+      propertiesForRent: propertyForRent?.hits,
+    },
+  };
 }
+
+export default Home;
--- react-realstateapp/utils/fetchApi.js
+++ react-realstateapp/utils/fetchApi.js
@@ -1,15 +1,14 @@
 import axios from "axios";
 
-/*   'x-rapidapi-host': 'bayut.p.rapidapi.com',
-    'x-rapidapi-key': '7b760b1092mshe837c1179b68733p13c26bjsn905c8dda768f' */
-
-const baseUrl = "http://bayut.p.rapidapi.com";
+export const baseUrl = "https://bayut.p.rapidapi.com";
 
 export const fetchApi = async (url) => {
-  const response = await axios.get(url, {
+  const { data } = await axios.get(url, {
     headers: {
       "x-rapidapi-host": "bayut.p.rapidapi.com",
-      "x-rapidapi-key": "7b760b1092mshe837c1179b68733p13c26bjsn905c8dda768f",
+      "x-rapidapi-key": "7b760b1092mshe837c1179b68733p13c26bjsn905c8dda768f", // process.env.NEXT_PUBLIC_RAPID_API_KEY,
     },
   });
+
+  return data;
 };

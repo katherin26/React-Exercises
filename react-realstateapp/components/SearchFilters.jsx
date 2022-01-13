92b4e02c437dda4d24d88663a90fe17b90048b8b
katherin <ft.katheochoa@gmail.com>
2022-01-13 18:45:05 -0500

    added filterData.js file

--- react-realstateapp/components/SearchFilters.jsx
+++ react-realstateapp/components/SearchFilters.jsx
@@ -12,9 +12,16 @@ import {
 import { useRouter } from "next/router";
 import { MdCancel } from "react-icons/md";
 import Image from "next/image";
+import { filterData, getFilterValues } from "../utils/filterData";
 
 const SearchFilters = () => {
-  return <div>SearchFilters</div>;
+  const [filters, setFilters] = useState({ filterData });
+
+  return (
+    <>
+      <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap"></Flex>
+    </>
+  );
 };
 
 export default SearchFilters;

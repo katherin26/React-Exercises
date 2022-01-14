0d99b6086f7295ebd653add9549031f0c2d522df
katherin <ft.katheochoa@gmail.com>
2022-01-13 19:49:42 -0500

    added getFilterValues()

--- react-realstateapp/components/SearchFilters.jsx
+++ react-realstateapp/components/SearchFilters.jsx
@@ -14,13 +14,45 @@ import { MdCancel } from "react-icons/md";
 import Image from "next/image";
 import { filterData, getFilterValues } from "../utils/filterData";
 
+//searchProperties =we have to pass in an object in square brackets we're going to say : filter.queryName
+//We have our select element inside of the select we're mapping over the specific  items or options we need to
+//render for each specific select
 const SearchFilters = () => {
-  const [filters, setFilters] = useState({ filterData });
+  const [filters, setFilters] = useState(filterData);
+
+  const searchProperties = (filterValues) => {
+    const path = router.pathname;
+    const { query } = router;
+
+    const values = getFilterValues(filterValues);
+
+    values.forEach((item) => {
+      query[item.name] = item.value;
+    });
+    router.push({ pathname: path, query });
+  };
 
   return (
-    <>
-      <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap"></Flex>
-    </>
+    <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
+      {filters.map((filter) => (
+        <Box key={filter.queryName}>
+          <Select
+            placeholder={filter.placeholder}
+            w="fit-content"
+            p="2"
+            onChange={(e) =>
+              searchProperties({ [filter.queryName]: e.target.value })
+            }
+          >
+            {filter?.items?.map((item) => (
+              <option value={item.value} key={item.value}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      ))}
+    </Flex>
   );
 };
 

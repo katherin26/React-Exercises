d6623948eb6f6365cd480e3663adf3cb04af5043
katherin <ft.katheochoa@gmail.com>
2022-01-17 23:26:58 -0500

    Added the useStyles hook

--- react-travelapp/src/components/Header/Header.jsx
+++ react-travelapp/src/components/Header/Header.jsx
@@ -4,7 +4,12 @@ import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
 import { mergeClasses } from "@material-ui/styles";
 import SearchIcon from "@material-ui/icons/Search";
 
+//import the style.js, the useStyles is a hook
+import useStyles from "./styles";
+
 const Header = () => {
+  const classes = useStyles();
+
   return (
     <AppBar position="static">
       <Toolbar className={mergeClasses.toolbar}>
@@ -15,17 +20,17 @@ const Header = () => {
           <Typography variant="h6" className={classes.title}>
             Explore new places
           </Typography>
-          <Autocomplete>
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder="Search..."
-                classes={{ root: classes.inputRoot, input: classes.inputInput }}
-              />
+          {/* <Autocomplete> */}
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
             </div>
-          </Autocomplete>
+            <InputBase
+              placeholder="Search..."
+              classes={{ root: classes.inputRoot, input: classes.inputInput }}
+            />
+          </div>
+          {/* </Autocomplete> */}
         </Box>
       </Toolbar>
     </AppBar>
--- react-travelapp/src/components/Header/styles.js
+++ react-travelapp/src/components/Header/styles.js
@@ -12,8 +12,8 @@ export default makeStyles((theme) => ({
   search: {
     position: "relative",
     borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    "&:hover": { backgroundColor: alpha(theme.palette.common.white, 0.25) },
+    //backgroundColor: alpha(theme.palette.common.white, 0.15),
+    //"&:hover": { backgroundColor: alpha(theme.palette.common.white, 0.25) },
     marginRight: theme.spacing(2),
     marginLeft: 0,
     width: "100%",

bc4382e2e6c50ee25784fed2b7255423a1613a06
katherin <ft.katheochoa@gmail.com>
2022-01-17 23:20:08 -0500

    added styles.js in Header component with makeStyles

---
+++ react-travelapp/src/components/Header/styles.js
@@ -0,0 +1,48 @@
+//NOTE: We need to import makeStyles , Now we need to export the function and we need a callback
+//function inside that returns an object, you can add the classes in css and js type.
+// That means that if you want to have a class name title you would create a title property that's going
+//to be an object and then you can set the color to be equal to red.
+import { makeStyles } from "@material-ui/core/styles";
+
+export default makeStyles((theme) => ({
+  title: {
+    color: "red",
+  },
+
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    "&:hover": { backgroundColor: alpha(theme.palette.common.white, 0.25) },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto",
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: { width: "20ch" },
+  },
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
+  },
+}));

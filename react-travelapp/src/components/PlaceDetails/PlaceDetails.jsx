1e254fd99d7d65c565a9dde6000e92e848249491
katherin <ft.katheochoa@gmail.com>
2022-01-21 16:40:56 -0500

    Added place details

--- react-travelapp/src/components/PlaceDetails/PlaceDetails.jsx
+++ react-travelapp/src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,7 +1,123 @@
 import React from "react";
+import {
+  Box,
+  Typography,
+  Button,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Chip,
+} from "@material-ui/core";
 
-const PlaceDetails = () => {
-  return <h1>Place Details</h1>;
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+import PhoneIcon from "@material-ui/icons/Phone";
+import Rating from "@material-ui/lab/Rating";
+import useStyles from "./styles";
+
+const PlaceDetails = ({ place, selected, refProp }) => {
+  const classes = useStyles();
+
+  if (selected)
+    refProp?.current?.scrollIntoView({ behavior: "smooth", block: "start" });
+
+  return (
+    <>
+      <Card elevation={6}>
+        <CardMedia
+          style={{ height: 250 }}
+          image={
+            place.photo
+              ? place.photo.images.large.url
+              : "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bonappetit.com%2Fstory%2Frestaurants-are-hanging-by-a-thread-edward-lee&psig=AOvVaw1X6dhGAhphID0a_-Tq_x7P&ust=1642653017601000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLj0yL_9vPUCFQAAAAAdAAAAABAV"
+          }
+          title={place.name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5">
+            {place.name}
+          </Typography>
+          <Box display="flex" justifyContent="space-between">
+            <Rating value={Number(place.rating)} readOnly />
+            <Typography gutterBottom variant="subtitle1">
+              {place.num_reviews} Reviews
+            </Typography>
+          </Box>
+          <Box display="flex" justifyContent="space-between">
+            <Typography variant="subtitle1">Price</Typography>
+            <Typography gutterBottom variant="subtitle1">
+              {place.price_level}
+            </Typography>
+          </Box>
+          <Box display="flex" justifyContent="space-between">
+            <Typography variant="subtitle1">Ranking</Typography>
+            <Typography gutterBottom variant="subtitle1">
+              {place.ranking}
+            </Typography>
+          </Box>
+          {place?.awards?.map((award) => (
+            <Box
+              my={1}
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <img src={award.images.small} alt={award.display_name} />
+              <Typography variant="subtitle2" color="textSecondary">
+                {award.display_name}
+              </Typography>
+            </Box>
+          ))}
+          {place?.cuisine?.map(({ name }) => {
+            <Chip
+              key={name}
+              size="small"
+              label={name}
+              className={classes.chip}
+            />;
+          })}
+          {place?.address && (
+            <Typography
+              gutterBottom
+              variant="subtitle2"
+              color="textSecondary"
+              className={classes.subtitle}
+            >
+              <LocationOnIcon /> {place.address}
+            </Typography>
+          )}
+          {place?.phone && (
+            <Typography
+              gutterBottom
+              variant="subtitle2"
+              color="textSecondary"
+              className={classes.spacing}
+            >
+              <PhoneIcon />
+              {place.phone}
+            </Typography>
+          )}
+          <CardActions>
+            <Button
+              size="small"
+              color="primary"
+              onClick={() => window.open(place.web_url, "_blank")}
+            >
+              Trip Advisor
+            </Button>
+
+            <Button
+              size="small"
+              color="primary"
+              onClick={() => window.open(place.website, "_blank")}
+            >
+              Website
+            </Button>
+          </CardActions>
+        </CardContent>
+      </Card>
+    </>
+  );
 };
 
 export default PlaceDetails;

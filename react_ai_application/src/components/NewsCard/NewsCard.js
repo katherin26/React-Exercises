49639ae4df37a48122f80ca7e04e2df20689b346
katherin <ft.katheochoa@gmail.com>
2022-01-24 00:25:01 -0500

    added NewsCard component

--- react_ai_application/src/components/NewsCard/NewsCard.js
+++ react_ai_application/src/components/NewsCard/NewsCard.js
@@ -1,10 +1,49 @@
 import React from "react";
+import {
+  Card,
+  CardActions,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from "@material-ui/core";
 
-const NewsCard = () => {
+//We are destructuring the article by using another pair of curly braces.
+const NewsCard = ({
+  article: { description, publishedAt, source, title, url, urlToImage },
+  i,
+}) => {
   return (
-    <>
-      <div>News Card</div>
-    </>
+    <Card>
+      <CardActionArea>
+        <CardMedia />
+        <div>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            component="h2"
+          ></Typography>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            component="h2"
+          ></Typography>
+        </div>
+        <Typography gutterBottom variant="h5"></Typography>
+        <CardContent>
+          <Typography
+            variant="body2"
+            color="textSecondary"
+            component="p"
+          ></Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary"></Button>
+        <Typography variant="h5" color="textSecondary"></Typography>
+      </CardActions>
+    </Card>
   );
 };
 

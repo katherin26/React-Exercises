bf9d7553a1bbe5dfed3d2fbcbab5420ebf567493
katherin <ft.katheochoa@gmail.com>
2022-01-24 00:03:34 -0500

    added NewsCard and NewsCards

--- react_ai_application/src/components/NewsCards/NewsCards.js
+++ react_ai_application/src/components/NewsCards/NewsCards.js
@@ -1,13 +1,19 @@
 import React from "react";
 import NewsCard from "../NewsCard/NewsCard";
+import { Grid, Grow, Typography } from "@material-ui/core";
 
 const NewsCards = ({ articles }) => {
   return (
-    <div>
-      {articles.map((article, i) => (
-        <NewsCards />
-      ))}
-    </div>
+    //in: if true, show the component , triggers the enter or exit animation.
+    <Grow in>
+      <Grid container alignItems="stretch" spacing={3}>
+        {articles.map((article, i) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} style={{ display: "flex" }}>
+            <NewsCard article={article} i={i} />
+          </Grid>
+        ))}
+      </Grid>
+    </Grow>
   );
 };
 

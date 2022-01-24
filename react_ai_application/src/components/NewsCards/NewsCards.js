07e3fcaa41507d863496f6adbf494b0c9750f9dc
katherin <ft.katheochoa@gmail.com>
2022-01-24 16:38:58 -0500

    Added card styles

--- react_ai_application/src/components/NewsCards/NewsCards.js
+++ react_ai_application/src/components/NewsCards/NewsCards.js
@@ -3,6 +3,14 @@ import NewsCard from "../NewsCard/NewsCard";
 import { Grid, Grow, Typography } from "@material-ui/core";
 import useStyles from "./styles.js";
 
+/*NOTE: we map over the infoCards array and in there we can specify the grid. 
+        inside we used the if statement for specifu =
+        if it has info then show this typography and inside we will put the info card, 
+        {infoCard.title.split(' ')},[2]  = we are dinamically grabbing what info are we getting .
+        so = infoCar.title split then into an array and grab  the last element which means categories,terms or sources.
+
+*/
+
 const infoCards = [
   { color: "#00838f", title: "Latest News", text: "Give me the latest news" },
   {
@@ -38,7 +46,34 @@ const NewsCards = ({ articles }) => {
           alignItems="stretch"
           spacing={3}
         >
-          test
+          {infoCards.map((infoCard) => (
+            <Grid
+              item
+              xs={12}
+              sm={6}
+              md={4}
+              lg={3}
+              className={classes.infoCard}
+            >
+              <div
+                className={classes.card}
+                style={{ backgroundColor: infoCard.color }}
+              >
+                <Typography variant="h5" component="h5">
+                  {infoCard.title}
+                </Typography>
+                {infoCard.info ? (
+                  <Typography variant="h6" component="h6">
+                    <strong>{infoCard.title.split(" ")[2]}</strong>: <br />
+                    {infoCard.info}
+                  </Typography>
+                ) : null}
+                <Typography variant="h6" component="h6">
+                  Try saying: <br /> <i>{infoCard.text}</i>
+                </Typography>
+              </div>
+            </Grid>
+          ))}
         </Grid>
       </Grow>
     );

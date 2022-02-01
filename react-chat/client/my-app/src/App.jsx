c3c6b09518e78498cbc61a316ad11ce47e21bed7
katherin <ft.katheochoa@gmail.com>
2022-01-31 23:22:47 -0500

    Added components folder

--- react-chat/client/my-app/src/App.jsx
+++ react-chat/client/my-app/src/App.jsx
@@ -3,12 +3,17 @@ import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
 
-const apikey = "n54zxeg3zknf";
+//we get this api in getStream.io
+const apiKey = "n54zxeg3zknf";
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>
-      <h1>Medical Pager Chat App</h1>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChanellListContainer />
+        <ChannelContainer />
+      </Chat>
     </div>
   );
 };

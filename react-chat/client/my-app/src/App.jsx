37bf368afb8a18e356c3a30fea1907f31965bfe5
katherin <ft.katheochoa@gmail.com>
2022-01-31 23:29:14 -0500

    added channel container

--- react-chat/client/my-app/src/App.jsx
+++ react-chat/client/my-app/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
+import { ChannelContainer, ChannelListContainer } from "./components";
 
 //we get this api in getStream.io
 const apiKey = "n54zxeg3zknf";
@@ -11,7 +12,7 @@ const App = () => {
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
-        <ChanellListContainer />
+        <ChannelListContainer />
         <ChannelContainer />
       </Chat>
     </div>

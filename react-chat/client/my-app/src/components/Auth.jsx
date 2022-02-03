eb695f229c75e091b937eb9612d8077bf2ec77b8
katherin <ft.katheochoa@gmail.com>
2022-02-03 14:34:18 -0500

    Auth component

--- react-chat/client/my-app/src/components/Auth.jsx
+++ react-chat/client/my-app/src/components/Auth.jsx
@@ -2,16 +2,113 @@ import React, { useState } from "react";
 import Cookies from "universal-cookie";
 import axios from "axios";
 
-import signinImage from "../assets/signuo.jpg";
+import signinImage from "../assets/signup.jpg";
 
+/* {isSignup && () = THis is the shorthand method for the ternary operator, use the ternary if you have 
+two things that you want to show but you simply say is sign up and the and and if you only need to show
+one thing based on this condition. }
+NOTE: Line 100, In this paragraph we want to show a different message depending on if user is sign up or
+if not so we can say if is isSignUp in that case we can display a message something like this is signUp 
+and that's going to be already have an account ? but if it's not then we can say don't have an account?
+we need to be able to switch between the modes and then we are gonne have a span element instead of that
+p element and it's going to have the onClick property which is going to say switchMode 
+
+*/
 const Auth = () => {
   const [isSignup, setIsSignup] = useState(false);
+
+  const handleChange = () => {};
+
+  const switchMode = () => {
+    setIsSignup((prevIsSignup) => !prevIsSignup);
+  };
 
   return (
     <div className="auth__form-container">
       <div className="auth__form-container_fields">
         <div className="auth__form-container_fields-content">
           <p>{isSignup ? "Sign Up" : "Sign In"}</p>
+          <form onSubmit={() => {}}>
+            {isSignup && (
+              <div className="auth__form-container_fields-content_input">
+                <label htmlFor="fullName">Full Name</label>
+                <input
+                  name="fullName"
+                  type="text"
+                  placeholder="Full Name"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            )}
+
+            <div className="auth__form-container_fields-content_input">
+              <label htmlFor="username">Username</label>
+              <input
+                name="username"
+                type="text"
+                placeholder="Username"
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            {isSignup && (
+              <div className="auth__form-container_fields-content_input">
+                <label htmlFor="fullName">Phone Number</label>
+                <input
+                  name="phoneNumber"
+                  type="text"
+                  placeholder="Phone Number"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            )}
+
+            {isSignup && (
+              <div className="auth__form-container_fields-content_input">
+                <label htmlFor="avatarURL">Avatar URL</label>
+                <input
+                  name="avatarURL"
+                  type="text"
+                  placeholder="Avatar URL"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            )}
+            <div className="auth__form-container_fields-content_input">
+              <label htmlFor="password">Password</label>
+              <input
+                name="password"
+                type="password"
+                placeholder="Password"
+                onChange={handleChange}
+                required
+              />
+            </div>
+            {isSignup && (
+              <div className="auth__form-container_fields-content_input">
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <input
+                  name="confirmPassword"
+                  type="password"
+                  placeholder="confirmPassword"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            )}
+          </form>
+          <div className="auth__form-container_fields-account">
+            <p>
+              {isSignup ? "Already have an account?" : "Don't have an account?"}
+              <span onClick={switchMode}>
+                {isSignup ? "Sign In" : "Sign Up"}
+              </span>
+            </p>
+          </div>
         </div>
       </div>
     </div>

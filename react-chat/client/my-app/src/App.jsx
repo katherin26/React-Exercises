299dc674383d62d21e63ff5936d3863ac4e27aeb
katherin <ft.katheochoa@gmail.com>
2022-02-04 17:25:38 -0500

    added props in channelContainer

--- react-chat/client/my-app/src/App.jsx
+++ react-chat/client/my-app/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
@@ -37,13 +37,28 @@ still not logged in.
 */
 
 const App = () => {
+  const [createType, setCreateType] = useState("");
+  const [isCreating, setIsCreating] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
   if (!authToken) return <Auth />;
 
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
-        <ChannelListContainer />
-        <ChannelContainer />
+        <ChannelListContainer
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          setCreateType={setCreateType}
+          setIsEditing={setIsEditing}
+        />
+        <ChannelContainer
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          isEditing={isEditing}
+          setIsEditing={setIsEditing}
+          createType={createType}
+        />
       </Chat>
     </div>
   );
--- react-chat/client/my-app/src/components/ChannelContainer.jsx
+++ react-chat/client/my-app/src/components/ChannelContainer.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import { Channel, userChatContext } from "stream-chat-react";
 import { ChannelInner, CreateChannel, EditChannel, TeamMessage } from "./";
 
-const ChannelContainer = () => {
+const ChannelContainer = ({
+  isCreating,
+  setIsCreating,
+  isEditing,
+  setIsEditing,
+  createType,
+}) => {
   /*NOTE: The information about the current specific channel. Then we need to know are we currently creating
   that channel then we have to show a specific message or a dashboard for creating that channel.
   So we have to have a variable called is creating and if we are creating we want to return a specific jsx.
@@ -12,12 +18,20 @@ const ChannelContainer = () => {
 
   const { channel } = userChatContext();
 
-  if(isCreating){
-    return ();
+  if (isCreating) {
+    return (
+      <div className="channel__container">
+        <CreateChannel createType={createType} setIsCreating={setIsCreating} />
+      </div>
+    );
   }
 
-  if(isEditing){
-    return ();
+  if (isEditing) {
+    return (
+      <div className="channel__container">
+        <EditChannel setIsEditing={setIsEditing} />
+      </div>
+    );
   }
 
   return <div>ChannelContainer</div>;

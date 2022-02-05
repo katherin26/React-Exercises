426411916bc8d5f366f9d216a8d120e871941f6a
katherin <ft.katheochoa@gmail.com>
2022-02-04 22:27:35 -0500

    Create Channel

--- react-chat/client/my-app/src/components/CreateChannel.jsx
+++ react-chat/client/my-app/src/components/CreateChannel.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { useChatContext } from "stream-chat-react";
+import { UserList } from "./";
+import { CloseCreateChannel } from "../assets";
+
+const ChannelNameInput = ({channelName = '', setChannelName}) => {
+  const handleChange = (event) => {
+    event.preventDefault();
+    setChannelName(event.target.value);
+  }
+
+  return(
+    <div className="channel-name-input__wrapper">
+      <p>Name</p>
+        <input value={} onChange={} placeholder="channel-name (no spaces)"></input>
+      <p>Add Members</p>
+    </div>
+  )
+}
 
 const CreateChannel = () => {
-  return <div></div>;
+  return <div>createchannelcomponent</div>;
 };
 
 export default CreateChannel;

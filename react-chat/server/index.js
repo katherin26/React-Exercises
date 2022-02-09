7509fb1d2bd862becc4475fba220eb3d4fd2710a
katherin <ft.katheochoa@gmail.com>
2022-02-09 00:56:46 -0500

    added if statement for member

--- react-chat/server/index.js
+++ react-chat/server/index.js
@@ -14,6 +14,7 @@ require("dotenv").config();
 //end point is going to be app.post and inside we can set up our route which is going to be simply /.
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
+const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
 const twilioClient = require("twilio")(accountSid, authToken);
 
 /*NOTE: This is going to allow us to make cross=origin requests */
@@ -35,14 +36,24 @@ app.post("/", (req, res) => {
   const { message, user: sender, type, members } = req.body;
 
   if (type === "message.new") {
-    members.forEach(({ user }) => {
-      if (!user.online) {
-        twilioClient.messages.create({
-          body: `You have a new message from ${message.user.fullName} - ${message.text}`,
-        });
-      }
-    });
+    members
+      .filter((member) => member.user_id !== sender.id)
+      .forEach(({ user }) => {
+        if (!user.online) {
+          twilioClient.messages
+            .create({
+              body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+              messagingServiceSid: messagingServiceSid,
+              to: user.phoneNumber,
+            })
+            .then(() => console.log("Message sent!"))
+            .catch((err) => console.log(err));
+        }
+      });
+
+    return res.status(200).send("Message sent!");
   }
+  return res.status(200).send("Not a new message request");
 });
 
 /*NOTE: ADD ROUTES */

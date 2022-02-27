c8c643e225005291ee706f861c1e8b7bf64902d3
Katherin26 <ft.katheochoa@gmail.com>
2022-02-27 18:00:53 -0500

    Set up backend and added mongoose

--- 18-react-Mern-app/Notes.md
+++ 18-react-Mern-app/Notes.md
@@ -1,6 +1,11 @@
 # **CLIENT SIDE**
 
 1. npx create-react-app ./
+2. Install dependencies: npm install
+   **Axios**: For making api requests.
+   **moment**: Is a library for working with time and date.
+   **react-file-base64**: This is gonna to convert our images.
+   **redux** and **redux-thunk**: redux thunk is used for asynchronous actions using redux.
 
 # **SERVER SIDE**
 
@@ -14,3 +19,11 @@
    that for us.
 3. Inside the index.js we import all the dependencies and we add in the package.json below main , "type":"module"
    and in scripts we add: "start": "nodemon index.js".
+4. mongodb.com/cloud/atlas: open a free account and create a new cluster.
+   go to Database Access and **add new database user**. create your username and password.
+   with this credentials we'll be able to read and write to any database.
+   Finally we need to go to **network access** and then in here you can click **add Ip address**.
+   Then comeback to cluster and click in **connect**. and choose connect your application, because we want to connect it to our server side express and node application you can copy this string and then go back to visual
+   studio.
+   Create a new variable which is going to be called CONNECTION_URL: then copy and paste the string.
+5. npm i dotenv
--- 18-react-Mern-app/server/index.js
+++ 18-react-Mern-app/server/index.js
@@ -2,3 +2,43 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+/*Connect our server application with a real database for that we are going to use mongodb more specifically their
+cloud atlas version of mongobd which means they are going to host our database on their cloud .*/
+
+const PORT = process.env.PORT || 5000;
+
+/*Mongoose = We use our mongoose to connect our database.
+mongoose.connect(); = is a function that accepts two different parameters first one is going to be CONNECTION URL,
+and the second one is an object with all the options, we're gonna use two different options.
+first: useNewUrlParser: true, useUnifiedTopology:true
+This return a promise = so, we need to have a then and a catch.
+Then : inside the then we need to have app.listen  and this has two parameters the first is the PORT and the second 
+one is going to be a callback application which is going to ran once our application succesfully listens. this is 
+going to be a simple console.log(`Server running on port: ${PORT}`).
+and finally if the connection to the database is not succesful we're going to have an error there and we are simply 
+going to console.log(`error.message`).
+and the final piece of setup is going to be mongoose.set and there we're going to set useFindAndModify to false.
+*/
+
+mongoose
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set("useFindAndModify", false);

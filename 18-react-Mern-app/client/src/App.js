6edb8226a53f4ff580a367ccd4291da7ded44b2b
Katherin26 <ft.katheochoa@gmail.com>
2022-02-27 23:47:14 -0500

    added useDispatch from react-redux

--- 18-react-Mern-app/client/src/App.js
+++ 18-react-Mern-app/client/src/App.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 import memories from "./images/memories.png";
 import useStyles from "./styles.js";
 
+/*NOTE: with useDispatch= we used this for dispatch an action. We need to define that dispatch so we need
+say, const this patch is equal to use dispatch that is a hook, now that we have access to this dispatch we need
+to find a way where we are actually going to dispatch the action and the best way to do that is inside of
+the useEffect()*/
 const App = () => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch();
+  }, []);
 
   return (
     <Container maxWidth="lg">

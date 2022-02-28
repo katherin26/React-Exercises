ee34248fe1feeb450f7c8dae484b7cb300d03e44
Katherin26 <ft.katheochoa@gmail.com>
2022-02-27 22:03:40 -0500

    added Form, Post, Posts components

--- 18-react-Mern-app/client/src/App.js
+++ 18-react-Mern-app/client/src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 import memories from "./images/memories.png";
+import Post from "./components/Posts/Post/Post";
 
 const App = () => {
   return (
@@ -11,6 +14,23 @@ const App = () => {
         </Typography>
         <img src={memories} alt="memories" height="60" />
       </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
     </Container>
   );
 };
---
+++ 18-react-Mern-app/client/src/components/Posts/Posts.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Post from "./Post/Post";
+
+function Posts() {
+  return (
+    <>
+      <div>Posts</div>
+      <Post />
+      <Post />
+    </>
+  );
+}
+
+export default Posts;

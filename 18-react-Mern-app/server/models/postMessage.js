17f2571ec45f6baa21d671cb8b3992077312efa0
Katherin26 <ft.katheochoa@gmail.com>
2022-02-27 19:13:46 -0500

    Added Models folder, postMessage.js = schema

---
+++ 18-react-Mern-app/server/models/postMessage.js
@@ -0,0 +1,30 @@
+/*Here we are going to utilize the possibilities of mongoose.
+first we have to create a mongoose schema : const postSchema
+What is a schema : with mongodb you can create documents that look absolutely different. one can have the title
+and the message what can only have the message and so on.
+mongoose allows us to give some sort of uniformity to our documents we are going to specify that each post is going
+to have  these things. 
+
+*/
+
+import mongoose from "mongoose";
+
+const postSchema = mongoose.Schema({
+  title: String,
+  message: String,
+  creator: String,
+  tags: [String],
+  selectedFile: String,
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
+
+const PostMessage = mongoose.model("PostMessage", postSchema);
+
+export default PostMessage;

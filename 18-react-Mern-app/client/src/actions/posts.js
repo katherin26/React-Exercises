57698fde303c40aa86eeb0876c925eb378b028cd
Katherin26 <ft.katheochoa@gmail.com>
2022-03-02 12:17:10 -0500

    added constants file

--- 18-react-Mern-app/client/src/actions/posts.js
+++ 18-react-Mern-app/client/src/actions/posts.js
@@ -1,3 +1,4 @@
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from "../constants/actionTypes";
 import * as api from "../api";
 
 /*Action Creators = are functions that return actions and that action is just an object that has the 
@@ -12,7 +13,7 @@ function in front of it and then instead of returning the action we have to disp
 export const getPosts = () => async (dispatch) => {
   try {
     const { data } = await api.fetchPosts();
-    dispatch({ type: "FETCH_ALL", payload: data });
+    dispatch({ type: FETCH_ALL, payload: data });
   } catch (error) {
     console.log(error.message);
   }
@@ -25,7 +26,7 @@ export const createPost = (post) => async (dispatch) => {
   try {
     const { data } = await api.createPost(post);
 
-    dispatch({ type: "CREATE", payload: data });
+    dispatch({ type: CREATE, payload: data });
   } catch (error) {
     console.log(error);
   }
@@ -34,7 +35,7 @@ export const createPost = (post) => async (dispatch) => {
 export const updatePost = (id, post) => async (dispatch) => {
   try {
     const { data } = await api.updatePost(id, post);
-    dispatch({ type: "UPDATE", payload: data });
+    dispatch({ type: UPDATE, payload: data });
   } catch (error) {
     console.log(error);
   }
@@ -43,7 +44,7 @@ export const updatePost = (id, post) => async (dispatch) => {
 export const deletePost = (id) => async (dispatch) => {
   try {
     await api.deletePost(id);
-    dispatch({ type: "DELETE", payload: id });
+    dispatch({ type: DELETE, payload: id });
   } catch (error) {
     console.log(error);
   }
@@ -52,7 +53,7 @@ export const deletePost = (id) => async (dispatch) => {
 export const likePost = (id) => async (dispatch) => {
   try {
     const { data } = await api.likePost(id);
-    dispatch({ type: "UPDATE", payload: data });
+    dispatch({ type: UPDATE, payload: data });
   } catch (error) {
     console.log(error);
   }
--- 18-react-Mern-app/client/src/reducers/posts.js
+++ 18-react-Mern-app/client/src/reducers/posts.js
@@ -6,19 +6,20 @@ Usually you are going to have multiple if statements for a lot of different thin
 people prefer to have the switch statement and this is how it works.
 */
 
+import { FETCH_ALL, CREATE, UPDATE, DELETE } from "../constants/actionTypes";
+
 export default (posts = [], action) => {
   switch (action.type) {
-    case "DELETE":
+    case DELETE:
       return posts.filter((post) => post.id !== action.payload);
-    case "UPDATE":
-    case "LIKE":
+    case UPDATE:
       return posts.map((post) =>
         post._id === action.payload._id ? action.payload : post
       );
-    case "FETCH_ALL":
+    case FETCH_ALL:
       return action.payload;
 
-    case "CREATE":
+    case CREATE:
       return [...posts, action.payload];
 
     default:

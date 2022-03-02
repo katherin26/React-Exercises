14e8b6985200dd1aa99c9dc8142c3bee00c61591
Katherin26 <ft.katheochoa@gmail.com>
2022-03-02 15:21:01 -0500

    added routes

--- 18-react-Mern-app/client/src/App.js
+++ 18-react-Mern-app/client/src/App.js
@@ -5,12 +5,17 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 
+/*NOTE: For use react-router-dom we need to wrap all inside the BrowserRouter and the use the Switch 
+tag to add the route path. */
 const App = () => {
   return (
     <BrowserRouter>
       <Container maxWidth="lg">
         <Navbar />
-        <Switch></Switch>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/Auth" exact component={Auth} />
+        </Switch>
         <Home />
       </Container>
     </BrowserRouter>

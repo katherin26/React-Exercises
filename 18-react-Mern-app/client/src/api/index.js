aa01a09ff3e5eedbb3c8015b5ec4d3ab7c81b8a1
Katherin26 <ft.katheochoa@gmail.com>
2022-03-05 20:51:40 -0500

    added sign in and sign out routes

--- 18-react-Mern-app/client/src/api/index.js
+++ 18-react-Mern-app/client/src/api/index.js
@@ -1,13 +1,23 @@
 import axios from "axios";
+const API = axios.create({ baseURL: "http://localhost:5000" });
 
-const url = "http://localhost:5000/posts";
-
-export const fetchPosts = () => axios.get(url);
-
+//const url = "http://localhost:5000/posts";
 /*Now, creating the memory we are going to able to add a new post into our database connected to our 
-server now we go to actions*/
-export const createPost = (newPost) => axios.post(url, newPost);
+server now we go to actions
+
+create an axios instance and that's going to be = to axios.create().
+THen replace all the axios for API , this is going to do the same thing.
+replace url for '/posts'
+*/
+
+export const fetchPosts = () => API.get("/posts");
+export const createPost = (newPost) => API.post("/posts", newPost);
+export const likePost = (id) => API.patch(`'/posts'/${id}/likePost`);
 export const updatePost = (id, updatedPost) =>
-  axios.patch(`${url}/${id}`, updatedPost);
-export const deletePost = (id) => axios.delete(`${url}/${id}`);
-export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+  API.patch(`'/posts'/${id}`, updatedPost);
+export const deletePost = (id) => API.delete(`'/posts'/${id}`);
+
+//Create the route for the sign in and sign out:
+
+export const signIn = (formData) => API.post("/users/signin", formData);
+export const signUp = (formData) => API.post("/users/signup", formData);

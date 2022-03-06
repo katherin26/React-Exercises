47157ae1b35c261b0316b7073c5c9c0068bfa743
Katherin26 <ft.katheochoa@gmail.com>
2022-03-06 14:51:05 -0500

    added API.interceptors for connect the token with the backend

--- 18-react-Mern-app/client/src/api/index.js
+++ 18-react-Mern-app/client/src/api/index.js
@@ -10,6 +10,35 @@ THen replace all the axios for API , this is going to do the same thing.
 replace url for '/posts'
 */
 
+/*This is going to help our odd middleware and that is adding something specific to each one of our 
+requests., this is going to be a function that's going to happen on each of our requests so inside the 
+parenthesis you provide a callback function.
+That callback function provides a request as the first parameter , so again this is going to happen before 
+all of these requests. 
+So why do we need to have this , because we have to send our token back to our backend so that 
+the backend middleware can verify that we are actually logged in for that we can say if(localstorage.getItem)
+and we want to get the item of profile that's where we store the token, so if that exists then we want to add
+something to our request and that something is going to be rec.headers.Authorization, if we go back to 
+the backend and we took to the middleware we can see we are taking something from  (const token = req.headers.authorization.split(' ')[1])
+That's why we need to put our token and the token is going to be a string and it needs to start with the word 
+'bearer', this is going to be a bare token the first thing is the bearer just a string and then the second thing 
+divided by a space. 
+Is the actual token, and we added with the .token
+return the req and that can make all these future requests.
+with that our backend is going to get a specific header and then based on that header we can do what we've
+donde here in (server/middleware/auth.js/auth).
+decode the data and based on that the back end is going to know that our user is indeed logged .
+
+*/
+API.interceptors.request.use((req) => {
+  if (localStorage.getItem("profile")) {
+    req.headers.Authorization = `Bearer ${
+      JSON.parse(localStorage.getItem("profile")).token
+    }`;
+  }
+  return req;
+});
+
 export const fetchPosts = () => API.get("/posts");
 export const createPost = (newPost) => API.post("/posts", newPost);
 export const likePost = (id) => API.patch(`'/posts'/${id}/likePost`);

8b66a58b75f10866bc46b7ceb42a915318e41e8c
Katherin26 <ft.katheochoa@gmail.com>
2022-03-07 16:39:55 -0500

    added Route paths /posts

--- 18-react-Mern-app/client/src/App.js
+++ 18-react-Mern-app/client/src/App.js
@@ -1,20 +1,34 @@
 import React from "react";
 import { Container } from "@material-ui/core";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Redirect } from "react-router-dom";
 
 import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Auth from "./components/Auth/Auth";
 
 /*NOTE: For use react-router-dom we need to wrap all inside the BrowserRouter and the use the Switch 
-tag to add the route path. */
+tag to add the route path. 
+  //NOTE: Pagination : we need to add Redirect component 
+
+  Inside the Route component , of all we're going to have a path just, which usually renders first on the 
+  page, we don't want to render a home component there, what we want to do is call a callback function 
+  which is going to call the redirect component and that redirect is going to redirect us to , and that's
+  going to be /posts, 
+  so we only want to see posts if we are on this specific path (path="/") and we 
+  are going to be immediately redirected if we do this. also we need to have a new tag router and is going 
+  to render the component called home in the path /posts/search
+  
+*/
 const App = () => {
   return (
     <BrowserRouter>
-      <Container maxWidth="lg">
+      <Container maxWidth="xl">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={() => <Redirect to="/posts" />} />
+          <Route path="/posts" element={<Home />} />
+          <Route path="/posts/search" element={<Home />} />
+          <Route path="/posts/:id" element={PostDetails} />
           <Route path="/auth" element={<Auth />} />
         </Routes>
       </Container>

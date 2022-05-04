bc8e7ae537e30af9a7f4fffc90231c208823a78a
Katherin26 <ft.katheochoa@gmail.com>
2022-05-03 20:15:00 -0400

    added classes into Home Function

--- 20-react-next-ecommerce-application/ecommerce/pages/index.js
+++ 20-react-next-ecommerce-application/ecommerce/pages/index.js
@@ -4,11 +4,13 @@ function Home() {
   return (
     <>
       Hero Banner
-      <div>
+      <div className="products-heading">
         <h2>Best gaming products</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div>{["Product 1", "Product 2"].map((product) => product)}</div>
+      <div className="products-container">
+        {["Product 1", "Product 2"].map((product) => product)}
+      </div>
       Footer
     </>
   );

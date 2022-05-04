63f7b59cef9fbfedc86f70b95778dd050d2ff40b
Katherin26 <ft.katheochoa@gmail.com>
2022-05-03 22:44:55 -0400

    Home component

--- 20-react-next-ecommerce-application/ecommerce/pages/index.js
+++ 20-react-next-ecommerce-application/ecommerce/pages/index.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { client } from "../lib/client";
 import { Product, FooterBanner, HeroBanner } from "./components";
 
-function Home() {
+/*IMPORTANT: For fetch data we use something known as get server side props.
+  if you export a function called getServerSideProps(Server-Side Rendering).
+  Next.js will pre-render this page on each request using the data returned by 
+  `getServerSideProps`.
+
+*/
+
+/*NOTE: if you're fetching data from an api or from a cms we can use the get server side
+   prop function, inside we need to form a sanity query,
+            '*[_type == "product"]' == let's grab all of the products from pur sanity 
+            dashboard
+   
+   */
+
+function Home({ products, bannerData }) {
   return (
     <>
       <HeroBanner />
@@ -10,11 +25,23 @@ function Home() {
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {["Product 1", "Product 2"].map((product) => product)}
+        {products?.map((product) => product.name)}
       </div>
       <FooterBanner />
     </>
   );
 }
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
 
 export default Home;

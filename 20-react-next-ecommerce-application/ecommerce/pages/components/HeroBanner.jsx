337763c79349230730115e89d003a6fadf9aefaf
Katherin26 <ft.katheochoa@gmail.com>
2022-05-03 20:53:44 -0400

    Hero Banner

--- 20-react-next-ecommerce-application/ecommerce/pages/components/HeroBanner.jsx
+++ 20-react-next-ecommerce-application/ecommerce/pages/components/HeroBanner.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Link from "next/link";
 
+//NOTE: LINK PROPERTY : is like an href but in next.js.
 function HeroBanner() {
-  return <div>HeroBanner</div>;
+  return (
+    <div className="hero-banner-container">
+      <div>
+        <p className="beats-solo">SMALL TEXT</p>
+        <h3>MID TEXT</h3>
+        <img src="" alt="headphones" className="hero-banner-image" />
+        <div>
+          <Link href="/product/ID">
+            <button type="button">BUTTON TEXT</button>
+          </Link>
+          <div className="desc">
+            <h5>Desscription</h5>
+            <p>DESCRIPTION</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default HeroBanner;

d3639deaa6af0054b8a851be6e165b0afd297c60
Katherin26 <ft.katheochoa@gmail.com>
2022-05-05 18:56:38 -0400

    added layout component

--- 20-react-next-ecommerce-application/ecommerce/pages/components/Layout.jsx
+++ 20-react-next-ecommerce-application/ecommerce/pages/components/Layout.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Head from "next/head";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 function Layout() {
-  return <div>Layout</div>;
+  return (
+    <div className="layout">
+      <Head>
+        <title>Katica's Store</title>
+      </Head>
+      <header>
+        <Navbar />
+      </header>
+      <main className="main-container">EMPTY</main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
 }
 
 export default Layout;

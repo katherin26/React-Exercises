2c2cf85f5a88665a5ecc3fd558d8100f452274d6
Katherin26 <ft.katheochoa@gmail.com>
2022-05-11 15:35:56 -0400

    added maylike component

--- 20-react-next-ecommerce-application/ecommerce/pages/product/[slug].js
+++ 20-react-next-ecommerce-application/ecommerce/pages/product/[slug].js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { client, urlFor } from "../components/lib/client";
 import {
   AiOutlineMinus,
@@ -6,6 +6,7 @@ import {
   AiFillStar,
   AiOutlineStar,
 } from "react-icons/ai";
+import { Product } from "../components";
 
 /*NOTE: [slug] That means is going to be dynamic 
         example:
@@ -26,23 +27,32 @@ to be statically generated.
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
+  const [index, setIndex] = useState(0);
   return (
     <div>
-      <div className="product-detail-container">
-        <div className="image-container">
-          <img
-            src={urlFor(image && image[0])}
-            className="product-detail-image"
-          />
+      <div>
+        <div className="product-detail-container">
+          <div className="image-container">
+            <img
+              src={urlFor(image && image[index])}
+              className="product-detail-image"
+            />
+          </div>
+          <div className="small-images-container">
+            {image?.map((item, i) => (
+              <img
+                src={urlFor(item)}
+                className={
+                  i === index ? "small-image selected-image" : "small-image"
+                }
+                onMouseEnter={() => setIndex(i)}
+              />
+            ))}
+          </div>
         </div>
-        {/*  <div className="small-images-container">
-          {image?.map((item, i) => (
-            <img src={urlFor(item)} className="" onMouseEnter="" />
-          ))}
-          </div>*/}
       </div>
 
-      <div className="product-details-desc">
+      <div className="product-detail-desc">
         <h1>{name}</h1>
         <div className="reviews">
           <div>
@@ -68,6 +78,25 @@ const ProductDetails = ({ product, products }) => {
               <AiOutlinePlus />
             </span>
           </p>
+        </div>
+        <div className="buttons">
+          <button type="button" className="add-to-cart" onClick="">
+            Add to Cart
+          </button>
+          <button type="button" className="buy-now" onClick="">
+            Buy Now
+          </button>
+        </div>
+      </div>
+
+      <div className="maylike-products-wrapper">
+        <h2>You may also like</h2>
+        <div className="marquee">
+          <div className="maylike-products-container track">
+            {products.map((item) => (
+              <Product key={item._id} product={item} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
@@ -115,11 +144,6 @@ export const getStaticProps = async ({ params: { slug } }) => {
   const productsQuery = '*[_type == "product"]';
   const product = await client.fetch(query);
   const products = await client.fetch(productsQuery);
-  console.log(`impresion =)`);
-  console.log(product);
-  console.log(query);
-  console.log(productsQuery);
-
   return {
     props: { products, product },
   };
